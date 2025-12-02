5d18c1df68248d8327136408c437af398f2a3510
Julian Köpke <julian.koepke@gmail.com>
2025-12-02 23:27:37 +0100

    add day03

---
+++ bin/day03.ml
@@ -0,0 +1,11 @@
+let rec read acc =
+  try
+    let line = input_line stdin in
+    read (line :: acc)
+  with End_of_file -> List.rev acc
+
+let parse lines = List.map (fun l -> l) lines
+
+let () =
+  let clicks = read [] |> parse in
+  List.iter (fun line -> Printf.printf "%s\n" line) clicks
